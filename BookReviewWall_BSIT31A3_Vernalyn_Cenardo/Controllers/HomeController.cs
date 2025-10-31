4dab916fd60b375559316f36b6f42ff4ca044c3b
FrancineMei <10cineeeee@gmail.com>
2025-10-31 09:23:18 +0800

    Initial commit: Project setup with solution structure, including .gitignore

---
+++ BookReviewWall_BSIT31A3_Vernalyn_Cenardo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using BookReviewWall_BSIT31A3_Vernalyn_Cenardo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookReviewWall_BSIT31A3_Vernalyn_Cenardo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BookReviewWall_BSIT31A3_Vernalyn_Cenardo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookReviewWall_BSIT31A3_Vernalyn_Cenardo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
